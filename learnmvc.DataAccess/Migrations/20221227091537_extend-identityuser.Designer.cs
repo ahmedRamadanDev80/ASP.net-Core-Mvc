f6d4aa8ece17750b88905668eb3585103b6a4b08
ramadan <ar80pro@gmail.com>
2022-12-27 11:20:46 +0200

    extend-identityuser

--- learnmvc.DataAccess/Migrations/20221227091537_extend-identityuser.Designer.cs
+++ learnmvc.DataAccess/Migrations/20221227091537_extend-identityuser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using learnmvc.DataAccess;
 
@@ -11,9 +12,11 @@ using learnmvc.DataAccess;
 namespace learnmvc.DataAccess.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221227091537_extend-identityuser")]
+    partial class extendidentityuser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

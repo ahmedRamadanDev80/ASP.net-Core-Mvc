941bd6b85af1e55c141cfcd8497e476c3fafd749
ramadan <ar80pro@gmail.com>
2022-12-27 20:20:43 +0200

    add-Roles

--- learnmvc.Utility/SD.cs
+++ learnmvc.Utility/SD.cs
@@ -8,5 +8,9 @@ namespace learnmvc.Utility
 {
     public static class SD
     {
+        public const string Role_User_indi = "Individual";
+        public const string Role_User_Comp = "Company";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
     }
 }
--- learnmvc/Program.cs
+++ learnmvc/Program.cs
@@ -3,6 +3,8 @@ using learnmvc.DataAccess.Repository;
 using learnmvc.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using learnmvc.Utility;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,8 +13,10 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(/*options => options.SignIn.RequireConfirmedAccount = true*/).AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddDefaultTokenProviders().AddEntityFrameworkStores<AppDbContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddSingleton<IEmailSender, EmailSender>();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

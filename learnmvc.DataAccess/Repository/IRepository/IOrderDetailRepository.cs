d77935fbe6f0b231503cb885a95bc227c233a68a
ramadan <ar80pro@gmail.com>
2023-01-10 20:32:38 +0200

    add OrderHeader and Details to UnitOfWork

---
+++ learnmvc.DataAccess/Repository/IRepository/IOrderDetailRepository.cs
@@ -0,0 +1,14 @@
+﻿using learnmvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learnmvc.DataAccess.Repository.IRepository
+{
+    public interface IOrderDetailRepository : IRepository<OrderDetail>
+    {
+        void Update(OrderDetail obj);
+    }
+}
---
+++ learnmvc.DataAccess/Repository/OrderDetailRepository.cs
@@ -0,0 +1,25 @@
+﻿using learnmvc.DataAccess.Repository.IRepository;
+using learnmvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learnmvc.DataAccess.Repository
+{
+    public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
+    {
+        private readonly AppDbContext _db;
+
+        public OrderDetailRepository(AppDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(OrderDetail obj)
+        {
+            _db.OrderDetails.Update(obj);
+        }
+    }
+}

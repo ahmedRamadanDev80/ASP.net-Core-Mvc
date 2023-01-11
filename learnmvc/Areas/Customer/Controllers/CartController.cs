f0427a2186bd97ecf07f01075fd8da97ad319811
ramadan <ar80pro@gmail.com>
2023-01-11 09:39:46 +0200

    Summary POST Action Method

--- learnmvc/Areas/Customer/Controllers/CartController.cs
+++ learnmvc/Areas/Customer/Controllers/CartController.cs
@@ -1,6 +1,7 @@
 ﻿using learnmvc.DataAccess.Repository.IRepository;
 using learnmvc.Models;
 using learnmvc.Models.ViewModels;
+using learnmvc.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
@@ -12,7 +13,8 @@ namespace learnmvc.Areas.Customer.Controllers
     public class CartController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ShoppingCartVM ShoppingCartVM { get; set; }
+		[BindProperty]
+		public ShoppingCartVM ShoppingCartVM { get; set; }
 		public int OrderTotal { get; set; }
 		public CartController(IUnitOfWork unitOfWork)
         {
@@ -65,6 +67,50 @@ namespace learnmvc.Areas.Customer.Controllers
 				ShoppingCartVM.OrderHeader.OrderTotal += (cart.Count * cart.Price);
 			}
 			return View(ShoppingCartVM);
+		}
+		[HttpPost]
+		[ActionName("Summary")]
+		[ValidateAntiForgeryToken]
+		public IActionResult SummaryPOST()
+		{
+			var claimsIdentity = (ClaimsIdentity)User.Identity;
+			var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+			ShoppingCartVM.ListCart = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value,
+				includeProperties: "Product");
+
+			ShoppingCartVM.OrderHeader.PaymentStatus = SD.PaymentStatusPending;
+			ShoppingCartVM.OrderHeader.OrderStatus = SD.StatusPending;
+			ShoppingCartVM.OrderHeader.OrderDate = System.DateTime.Now;
+			ShoppingCartVM.OrderHeader.ApplicationUserId = claim.Value;
+
+
+			foreach (var cart in ShoppingCartVM.ListCart)
+			{
+				cart.Price = getPriceBasedOnQuantity(cart.Count, cart.Product.Price,
+					cart.Product.Price50, cart.Product.Price100);
+				ShoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
+			}
+
+
+			_unitOfWork.OrderHeader.Add(ShoppingCartVM.OrderHeader);
+			_unitOfWork.Save();
+			foreach (var cart in ShoppingCartVM.ListCart)
+			{
+				OrderDetail orderDetail = new()
+				{
+					ProductId = cart.ProductId,
+					OrderId = ShoppingCartVM.OrderHeader.Id,
+					Price = cart.Price,
+					Count = cart.Count
+				};
+				_unitOfWork.OrderDetail.Add(orderDetail);
+				_unitOfWork.Save();
+			}
+
+			_unitOfWork.ShoppingCart.RemoveRange(ShoppingCartVM.ListCart);
+			_unitOfWork.Save();
+			return RedirectToAction("Index", "Home");
 		}
 		public IActionResult plus(int cartId)
         {
